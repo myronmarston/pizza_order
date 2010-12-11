c35da417611dda2ded11ac84d52a028608714593
Myron Marston <myron.marston@gmail.com>
2010-12-11 10:47:46 -0800

    Added Piecora's menu.

---
+++ lib/pizza_order/piecoras.rb
@@ -0,0 +1,16 @@
+module PizzaOrder
+  module Piecoras
+    MENU = [
+      Pizza.new("The Veggie", :mushrooms, :onions, :olives, :green_peppers),
+      Pizza.new("The Combo", :salami, :pepperoni, :italian_sausage, :canadian_bacon, :mushrooms, :onions, :olives, :green_peppers, :tomatoes),
+      Pizza.new("Sweet Italian", :sweet_fried_peppers, :italian_sausage, :garlic),
+      Pizza.new("Queens", :italian_sausage, :pepperoni, :meatballs),
+      Pizza.new("Manhattan", :artichoke_hearts, :olives, :mushrooms, :coppacola),
+      Pizza.new("Brooklyn", :pepperoni, :mushrooms, :olives),
+      Pizza.new("Jeffro's Veggie", :sun_dried_tomatoes, :zucchini, :broccoli, :onions, :spinach),
+      Pizza.new("Gerry's Own", :italian_sausage, :feta_cheese, :roasted_peppers),
+      Pizza.new("The Greek", :feta_cheese, :olives, :onions, :tomatoes)
+      Pizza.new("Westcoaster", :canadian_bacon, :pineapple)
+    ]
+  end
+end
